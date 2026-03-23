619816094213f974c865389012e6470d0cc7193b
samtari1 <samtariyang@gmail.com>
2026-03-22 23:02:44 -0400

    Initial Commit

---
+++ HumanBodyAnalysis3FacialFeatures/HumanBodyAnalysis3FacialFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanBodyAnalysis3FacialFeaturesApp.swift
+//  HumanBodyAnalysis3FacialFeatures
+//
+//  Created by Quanpeng Yang on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct HumanBodyAnalysis3FacialFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HumanBodyAnalysis3FacialFeaturesTests/HumanBodyAnalysis3FacialFeaturesTests.swift
@@ -0,0 +1,17 @@
+//
+//  HumanBodyAnalysis3FacialFeaturesTests.swift
+//  HumanBodyAnalysis3FacialFeaturesTests
+//
+//  Created by Quanpeng Yang on 3/22/26.
+//
+
+import Testing
+@testable import HumanBodyAnalysis3FacialFeatures
+
+struct HumanBodyAnalysis3FacialFeaturesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
